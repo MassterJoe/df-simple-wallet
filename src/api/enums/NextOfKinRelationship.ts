8546ecc40304302fad308a901dc971ad4fa478e2
Salawu O. Joseph <sojbaba98@gmail.com>
2025-02-13 17:00:21 +0100

    User repository done

---
+++ src/api/enums/NextOfKinRelationship.ts
@@ -0,0 +1,15 @@
+export enum NextOfKinRelationship {
+    FATHER = "father",
+    MOTHER = "mother",
+    BROTHER = "brother",
+    SISTER = "sister",
+    UNCLE = "uncle",
+    AUNT = "aunt",
+    COUSIN = "cousin",
+    NEPHEW = "nephew",
+    NIECE = "niece",
+    SON = "son",
+    DAUGHTER = "daugther",
+}
+
+// an enum is for you to set available options
